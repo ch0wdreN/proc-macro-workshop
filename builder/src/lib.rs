0c53a5f004b7fcc8e1084103b39f3c043d2a3f3c
ch0wdreN <i19yanagi@gmail.com>
2024-09-28 02:43:14 +0900

    feat: create CommandBuilder structure and implement builder function
    passed 02-create-builder.rs

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,28 @@
 use proc_macro::TokenStream;
+use quote::quote;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let _ = input;
 
-    TokenStream::new()
+    let expanded = quote! {
+        pub struct CommandBuilder {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+
+        impl Command {
+            pub fn builder() -> CommandBuilder {
+                CommandBuilder {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None
+                }
+            }
+        }
+    };
+    TokenStream::from(expanded)
 }
