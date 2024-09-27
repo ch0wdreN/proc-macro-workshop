80722042508154e565d2228aeb6f98853b8db4ce
ch0wdreN <i19yanagi@gmail.com>
2024-09-28 02:48:01 +0900

    modify: update on step 2 extract struct name and generate builder name
    passed 02-create-builder.rs

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,12 +1,16 @@
 use proc_macro::TokenStream;
+use proc_macro2::{Ident, Span};
 use quote::quote;
+use syn::{DeriveInput, parse_macro_input};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
+    let struct_name = input.ident;
+    let builder_name = Ident::new(&format!("{}Builder", struct_name), Span::call_site());
 
     let expanded = quote! {
-        pub struct CommandBuilder {
+        pub struct #builder_name {
             executable: Option<String>,
             args: Option<Vec<String>>,
             env: Option<Vec<String>>,
@@ -14,8 +18,8 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
 
         impl Command {
-            pub fn builder() -> CommandBuilder {
-                CommandBuilder {
+            pub fn builder() -> #builder_name {
+                #builder_name {
                     executable: None,
                     args: None,
                     env: None,
